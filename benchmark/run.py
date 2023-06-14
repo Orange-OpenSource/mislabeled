d9a7bc32e9aa42574823c235d735f0f126bc84fa
Thomas George <tfjgeorge@gmail.com>
2023-06-14 18:37:07 +0200

    adds AUM detector

--- benchmark/run.py
+++ benchmark/run.py
@@ -1,7 +1,10 @@
 # %%
 from sklearn.datasets import make_moons
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from mislabeled.detect import ConsensusDetector
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.pipeline import make_pipeline
+from mislabeled.detect import AUMDetector, ConsensusDetector, InfluenceDetector
 from mislabeled.filtering import FilterClassifier
 from bqlearn.corruptions import make_label_noise
 from sklearn.model_selection import cross_val_score, GridSearchCV
@@ -9,57 +12,83 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # %%
-X, y = make_moons(noise=.2)
-X_test, y_test = make_moons(noise=.2, n_samples=1000)
-y_corr = make_label_noise(y, noise_matrix="uniform", noise_ratio=0.2)
 
 cc_detect = ConsensusDetector(classifier=KNeighborsClassifier(n_neighbors=3))
-clf = FilterClassifier(detector=cc_detect, classifier=KNeighborsClassifier(n_neighbors=3))
+clf_cc = FilterClassifier(detector=cc_detect, classifier=KNeighborsClassifier(n_neighbors=3))
 
-param_grid = {
+param_grid_consensus = {
     'trust_proportion': [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.],
     'detector__n_cvs': [4, 5, 6]
 }
-scores = cross_val_score(clf, X, y_corr, cv=5)
 
-print(scores)
+cc_detect_aum = AUMDetector(classifier=AdaBoostClassifier())
+clf_aum = FilterClassifier(detector=cc_detect_aum, classifier=KNeighborsClassifier(n_neighbors=3))
+
+param_grid_aum = {
+    'trust_proportion': [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.],
+}
+
+cc_detect_influence = InfluenceDetector(transform=RBFSampler(gamma='scale'))
+clf_influence = FilterClassifier(detector=cc_detect_influence, classifier=KNeighborsClassifier(n_neighbors=3))
+
+param_grid_influence = {
+    'trust_proportion': [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.],
+    'detector__alpha': [.1, 1, 10],
+    'detector__transform__n_components': [100, 1000],
+}
+
+
 # %%
 
-gs_cv = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5)
-gs_cv.fit(X, y_corr)
+benchmark = {
+    'naive': {
+        'classifier': KNeighborsClassifier(n_neighbors=3),
+        'param_grid': {},
+    },
+    'consensus': {
+        'classifier': clf_cc,
+        'param_grid': param_grid_consensus,
+    },
+    'aum': {
+        'classifier': clf_aum,
+        'param_grid': param_grid_aum,
+    },
+    'influence': {
+        'classifier': clf_influence,
+        'param_grid': param_grid_influence,
+    },
+}
 
-print(gs_cv.cv_results_)
-print(gs_cv.cv_results_['params'])
+for k, d in benchmark.items():
+    benchmark[k]['grid_search'] = GridSearchCV(estimator=d['classifier'], param_grid=d['param_grid'], cv=5)
 
-# plt.plot(param_grid['trust_proportion'],
-#          gs_cv.cv_results_['mean_test_score'])
 # %%
 
-gs_cv.score(X_test, y_test)
+test_scores = {k: [] for k in benchmark.keys()}
 
-# %%
-
-print(KNeighborsClassifier(n_neighbors=3).fit(X, y_corr).score(X_test, y_test))
-# %%
-
-test_score_filter = []
-test_score_naive = []
-
-for i in range(20):
+for i in range(21):
 
     X, y = make_moons(noise=.2)
+    X_test, y_test = make_moons(noise=.2, n_samples=1000)
+
     y_corr = make_label_noise(y, noise_matrix="uniform", noise_ratio=0.2)
 
-    test_score_filter.append(gs_cv.fit(X, y_corr).score(X_test, y_test))
-    test_score_naive.append(KNeighborsClassifier(n_neighbors=3).fit(X, y_corr).score(X_test, y_test))
+    for k, d in benchmark.items():
+        test_scores[k].append(d['grid_search'].fit(X, y_corr).score(X_test, y_test))
 
-    print(gs_cv.best_params_)
+    print(i, [d[-1] for d in test_scores.values()])
+
 # %%
-print('filter', np.mean(test_score_filter), np.std(test_score_filter))
-print('naive', np.mean(test_score_naive), np.std(test_score_naive))
+
+for k, d in test_scores.items():
+    print(k, np.mean(d), np.std(d))
 # %%
-plt.violinplot([test_score_filter, test_score_naive])
-plt.xticks([1, 2], ['filter', 'naive'])
+
+plt.violinplot(test_scores.values(), showmedians=True)
+plt.xticks([1, 2, 3], test_scores.keys())
 plt.ylabel('test accuracy')
+plt.grid()
 plt.show()
+# %%
+
 # %%
--- mislabeled/detect.py
+++ mislabeled/detect.py
@@ -62,3 +62,115 @@ class ConsensusDetector(BaseEstimator):
                 consistent_label[i_val] += (y[i_val] == y_pred).astype(int)
 
         return consistent_label / self.n_cvs
+    
+
+class AUMDetector(BaseEstimator):
+    """ A template estimator to be used as a reference implementation.
+
+    For more information regarding how to build your own estimator, read more
+    in the :ref:`User Guide <user_guide>`.
+
+    Parameters
+    ----------
+    demo_param : str, default='demo_param'
+        A parameter used for demonstation of how to pass and store paramters.
+
+    Examples
+    --------
+    >>> from mislabeled import AUMDetector
+    >>> import numpy as np
+    >>> X = np.arange(100).reshape(100, 1)
+    >>> y = np.zeros((100, ))
+    >>> estimator = TemplateEstimator()
+    >>> estimator.fit(X, y)
+    TemplateEstimator()
+    """
+    def __init__(self, classifier=None):
+        self.classifier = classifier
+
+    def trust_score(self, X, y):
+        """A reference implementation of a fitting function.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels in classification, real numbers in
+            regression).
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        X, y = check_X_y(X, y, accept_sparse=True)
+        n = X.shape[0]
+
+        clf = self.classifier
+
+        clf.fit(X, y)
+        preds = np.zeros((clf.n_estimators, n))
+
+        for i, y_pred in enumerate(clf.staged_decision_function(X)):
+            preds[i] = y_pred * (y - .5) # TODO multiclass
+
+        return preds.sum(axis=0)
+    
+
+class InfluenceDetector(BaseEstimator):
+    """ A template estimator to be used as a reference implementation.
+
+    For more information regarding how to build your own estimator, read more
+    in the :ref:`User Guide <user_guide>`.
+
+    Parameters
+    ----------
+    demo_param : str, default='demo_param'
+        A parameter used for demonstation of how to pass and store paramters.
+
+    Examples
+    --------
+    >>> from mislabeled import AUMDetector
+    >>> import numpy as np
+    >>> X = np.arange(100).reshape(100, 1)
+    >>> y = np.zeros((100, ))
+    >>> estimator = TemplateEstimator()
+    >>> estimator.fit(X, y)
+    TemplateEstimator()
+    """
+    def __init__(self, alpha=1., transform=None):
+        self.alpha = alpha
+        self.transform = transform
+
+    def trust_score(self, X, y):
+        """A reference implementation of a fitting function.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            The training input samples.
+        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
+            The target values (class labels in classification, real numbers in
+            regression).
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        X, y = check_X_y(X, y, accept_sparse=True)
+        if self.transform is None:
+            X_t = X
+        else:
+            X_t = self.transform.fit_transform(X)
+            
+        n = X_t.shape[0]; d = X_t.shape[1]
+
+        inv = np.linalg.inv(X_t.T @ X_t + np.identity(d) * self.alpha)
+        H = X_t @ inv @ X_t.T
+        y_cent = y - .5 # TODO multiclass
+        
+        J = y_cent.reshape(-1, 1) * H * y_cent.reshape(1, -1)
+        m = (J * (y.reshape(-1, 1) == y.reshape(1, -1))).sum(axis=1)
+        return m
