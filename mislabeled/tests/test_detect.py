30aa673a32d2a04693fbd903bbe2aa63d9b6a37c
Thomas George <tfjgeorge@gmail.com>
2023-06-20 15:57:23 +0200

    moves simple test logic outside of test_aum_multiclass

--- mislabeled/tests/test_detect.py
+++ mislabeled/tests/test_detect.py
@@ -8,8 +8,9 @@ from sklearn.ensemble import GradientBoostingClassifier
 from mislabeled.detect import AUMDetector
 
 
-@pytest.mark.parametrize("n_classes", [2, 5])
-def test_aum_multiclass(n_classes):
+def simple_detect_test(n_classes, detector):
+    # a very simple task with a single mislabeled example that
+    # should be easily detected by every detection method
     seed = 1
     n_samples = 1000
 
@@ -25,8 +26,12 @@ def test_aum_multiclass(n_classes):
     index = random.randint(0, n_samples)
     y[index] = random.choice(list(filter(lambda c: c != y[index], np.unique(y))))
 
-    trust_scores = AUMDetector(
-        GradientBoostingClassifier(max_depth=1, n_estimators=20)
-    ).trust_score(X, y)
+    trust_scores = detector.trust_score(X, y)
 
     assert np.argmin(trust_scores) == index
+
+
+@pytest.mark.parametrize("n_classes", [2, 5])
+def test_aum_multiclass(n_classes):
+    detector = AUMDetector(GradientBoostingClassifier(max_depth=1, n_estimators=20))
+    simple_detect_test(n_classes, detector)
