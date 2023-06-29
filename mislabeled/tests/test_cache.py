b23ba00231c896cd2fd7eddc382d331c021b0cf1
Thomas George <tfjgeorge@gmail.com>
2023-06-29 11:48:48 +0200

    factors out task blobs_1_mislabeled

--- mislabeled/tests/test_cache.py
+++ mislabeled/tests/test_cache.py
@@ -1,34 +1,17 @@
 import time
 
 import numpy as np
-from sklearn.datasets import make_blobs
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 
 from mislabeled.detect import ConsensusDetector
 from mislabeled.filtering import FilterClassifier
 
+from .common import blobs_1_mislabeled
+
 
 def test_caching():
-    seed = 1
-    n_samples = 1000
-    n_classes = 2
-
-    X, y = make_blobs(
-        n_samples=n_samples,
-        centers=n_classes,
-        cluster_std=0.5,
-        random_state=seed,
-    )
-
-    np.random.seed(seed)
-
-    # picks one example of each class, and flips its label to the next class
-    indices_mislabeled = []
-    for c in range(n_classes):
-        index = np.random.choice(np.nonzero(y == c)[0])
-        indices_mislabeled.append(index)
-        y[index] = (y[index] + 1) % n_classes
+    X, y, _ = blobs_1_mislabeled(n_classes=2)
 
     base_classifier = KNeighborsClassifier(n_neighbors=3)
     classifier_detect = ConsensusDetector(base_classifier)
