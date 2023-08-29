f7e4ef1beded0c15bcb0e146a8157b4b5450a9a3
Pierre Nodet <pierre.nodet@orange.com>
2023-08-29 18:36:27 +0200

    correct plot

--- examples/plot_vog.py
+++ examples/plot_vog.py
@@ -1,3 +1,9 @@
+"""
+=====================
+Variance of Gradients
+=====================
+"""
+
 # %%
 
 import matplotlib.pyplot as plt
@@ -16,20 +22,33 @@ X, y = make_blobs(
 
 X = StandardScaler().fit_transform(X)
 
-mlp = MLPClassifier(hidden_layer_sizes=10, random_state=1)
-vog = VoGDetector(clone(mlp), n_directions=20, random_state=1)
+mlp = MLPClassifier(
+    solver="sgd",
+    activation="identity",
+    max_iter=15,
+    hidden_layer_sizes=(10,),
+    random_state=1,
+)
+vog = VoGDetector(clone(mlp), n_directions=10, random_state=1)
 
 mlp.fit(X, y)
 
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
 
-disp = DecisionBoundaryDisplay.from_estimator(mlp, X, response_method="predict_proba")
-disp.ax_.scatter(X[:, 0], X[:, 1], c=y)
+disp = DecisionBoundaryDisplay.from_estimator(
+    mlp, X, response_method="predict", ax=ax[0]
+)
+ax[0].scatter(X[:, 0], X[:, 1], c=y)
+ax[0].set_xlabel("Feature1")
+ax[0].set_ylabel("Feature1")
+ax[0].set_title("Toy Dataset trained decision boundary")
 
-# %%
 vog_scores = vog.trust_score(X, y)
-# %%
 p = mlp.predict_proba(X)[:, 1]
 dist_to_decision_boundary = np.abs(-np.log((1 / (p + 1e-8)) - 1))
-plt.scatter(vog_scores, dist_to_decision_boundary, c=y)
 
-# %%
+ax[1].scatter(vog_scores, dist_to_decision_boundary, c=y)
+ax[1].set_xlabel("VoG scores")
+ax[1].set_ylabel("Distances to Hyperplane")
+ax[1].set_title("Distance vs VoG score")
+plt.show()
