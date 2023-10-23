b16494874e386578bdaa37ca85f073f4b544ba2f
Pierre Nodet <pierre.nodet@orange.com>
2023-10-23 16:24:59 +0200

    change assertions in test for python3.8

--- mislabeled/tests/test_preprocessing.py
+++ mislabeled/tests/test_preprocessing.py
@@ -6,7 +6,7 @@ from mislabeled.preprocessing import WeakLabelEncoder
 def test_weak_label_encoder():
     Y_weak = [[0, 0, 1], [-1, 1, 1]]
     y_encoded = WeakLabelEncoder().fit_transform(Y_weak)
-    assert set(y_encoded) == set([0, 1])
+    np.testing.assert_array_equal(np.unique(y_encoded), [0, 1])
 
 
 def test_weak_label_encoder_strings():
@@ -14,8 +14,8 @@ def test_weak_label_encoder_strings():
     wle = WeakLabelEncoder().fit(Y_weak)
     y_encoded = wle.transform(Y_weak)
     y_recovered = wle.inverse_transform(y_encoded)
-    assert set(y_encoded) == set([0, 1])
-    assert set(y_recovered) == set(["a", "b"])
+    np.testing.assert_array_equal(np.unique(y_encoded), [0, 1])
+    np.testing.assert_array_equal(np.unique(y_recovered), ["a", "b"])
 
 
 def test_weak_label_encoder_missing():
@@ -23,4 +23,4 @@ def test_weak_label_encoder_missing():
     Y_weak[0, :] = np.array([0, 1])
     wle = WeakLabelEncoder(random_state=1).fit(Y_weak)
     y_encoded = wle.transform(Y_weak)
-    assert set(y_encoded) == set([0, 1])
+    np.testing.assert_array_equal(np.unique(y_encoded), [0, 1])
