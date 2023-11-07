44929d7d70525f1ce6071ebd5e845a369aefbc0b
Pierre Nodet <pierre.nodet@orange.com>
2023-11-07 17:41:00 +0100

    speed improvements

--- mislabeled/ensemble/_progressive.py
+++ mislabeled/ensemble/_progressive.py
@@ -1,13 +1,17 @@
 import copy
 import numbers
 from functools import singledispatch
+from itertools import islice
 
 import numpy as np
 from sklearn.base import clone
-from sklearn.ensemble import GradientBoostingClassifier, HistGradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.ensemble import (
+    AdaBoostClassifier,
+    GradientBoostingClassifier,
+    HistGradientBoostingClassifier,
+)
+from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
 from mislabeled.probe import check_probe
@@ -16,78 +20,68 @@ from ._base import AbstractEnsemble
 
 
 @singledispatch
-def incremental_fit(estimator, X, y, remaining_iterations, init=False):
-    return NotImplementedError
+def staged_fit(estimator, X, y):
+    raise NotImplementedError(
+        f"{estimator.__class__.__name__} doesn't support staged"
+        " learning. Register the estimator class to staged_fit."
+    )
 
 
-@incremental_fit.register(SGDClassifier)
-@incremental_fit.register(LogisticRegression)
-@incremental_fit.register(LinearSVC)
-def _incremental_fit_gradient(estimator, X, y, remaining_iterations, init=False):
-    if init:
-        estimator.fit(X, y)
-        n_classes = len(estimator.classes_)
-        if n_classes > 2:
-            n_iter = np.max(estimator.n_iter_)
-        else:
-            n_iter = estimator.n_iter_
-        remaining_iterations = [1] * n_iter
-        estimator.set_params(warm_start=False)
-
-    estimator.set_params(max_iter=remaining_iterations[0])
+@staged_fit.register(HistGradientBoostingClassifier)
+def _staged_fit_hgb(estimator: HistGradientBoostingClassifier, X, y):
     estimator.fit(X, y)
-    estimator.set_params(warm_start=True)
-
-    return estimator, remaining_iterations[1:]
-
-
-@incremental_fit.register(HistGradientBoostingClassifier)
-def _incremental_fit_hgb(
-    estimator: HistGradientBoostingClassifier, X, y, remaining_iterations, init=False
-):
-    if init:
-        estimator.fit(X, y)
-        remaining_iterations = []
-        for i in range(estimator.n_iter_):
-            shrinked = copy.deepcopy(estimator)
-            shrinked._predictors = estimator._predictors[0 : i + 1]
-            shrinked.train_score_ = estimator.train_score_[0 : i + 1]
-            shrinked.validation_score_ = estimator.validation_score_[0 : i + 1]
-            remaining_iterations.append(shrinked)
-
-    return remaining_iterations[0], remaining_iterations[1:]
+    shrinked = copy.deepcopy(estimator)
+    for i in range(estimator.n_iter_):
+        shrinked._predictors = estimator._predictors[0 : i + 1]
+        shrinked.train_score_ = estimator.train_score_[0 : i + 1]
+        shrinked.validation_score_ = estimator.validation_score_[0 : i + 1]
+        yield shrinked
 
 
-@incremental_fit.register(GradientBoostingClassifier)
-def _incremental_fit_gb(
-    estimator: GradientBoostingClassifier, X, y, remaining_iterations, init=False
-):
-    if init:
-        estimator.fit(X, y)
-        remaining_iterations = []
-        for i in range(estimator.n_estimators_):
-            shrinked = copy.deepcopy(estimator)
-            shrinked.estimators_ = estimator.estimators_[0 : i + 1]
-            if estimator.get_params()["subsample"] < 1:
-                shrinked.oob_improvement_ = estimator.oob_improvement_[0 : i + 1]
-                shrinked.oob_scores_ = estimator.oob_scores_[0 : i + 1]
-                shrinked.oob_score_ = estimator.oob_scores_[i]
-            shrinked.train_score_ = estimator.train_score_[0 : i + 1]
-            remaining_iterations.append(shrinked)
-
-    return remaining_iterations[0], remaining_iterations[1:]
-
-
-@incremental_fit.register(DecisionTreeClassifier)
-def _incremental_fit_tree(estimator, X, y, remaining_iterations, init=False):
-    if init:
-        path = estimator.cost_complexity_pruning_path(X, y)
-        remaining_iterations = list(reversed(path.ccp_alphas))
-
-    estimator.set_params(ccp_alpha=remaining_iterations[0])
+@staged_fit.register(GradientBoostingClassifier)
+def _staged_fit_gb(estimator: GradientBoostingClassifier, X, y):
     estimator.fit(X, y)
+    shrinked = copy.deepcopy(estimator)
+    for i in range(estimator.n_estimators_):
+        shrinked.estimators_ = estimator.estimators_[0 : i + 1]
+        if estimator.get_params()["subsample"] < 1:
+            shrinked.oob_improvement_ = estimator.oob_improvement_[0 : i + 1]
+            shrinked.oob_scores_ = estimator.oob_scores_[0 : i + 1]
+            shrinked.oob_score_ = estimator.oob_scores_[i]
+        shrinked.train_score_ = estimator.train_score_[0 : i + 1]
+        yield shrinked
 
-    return estimator, remaining_iterations[1:]
+
+@staged_fit.register(AdaBoostClassifier)
+def _staged_fit_ada(estimator: AdaBoostClassifier, X, y):
+    estimator.fit(X, y)
+    shrinked = copy.deepcopy(estimator)
+    for i in range(len(estimator.estimators_)):
+        shrinked.estimators_ = estimator.estimators_[0 : i + 1]
+        shrinked.estimator_weights_ = estimator.estimator_weights_[0 : i + 1]
+        shrinked.estimator_errors_ = estimator.estimator_errors_[0 : i + 1]
+        yield shrinked
+
+
+@staged_fit.register(SGDClassifier)
+def _staged_fit_sgd(estimator: SGDClassifier, X, y):
+    original = copy.deepcopy(estimator)
+    original.fit(X, y)
+    for i in range(original.n_iter_):
+        estimator.set_params(max_iter=1)
+        estimator.fit(X, y)
+        if i == 0:
+            estimator.set_params(warm_start=True)
+        yield estimator
+
+
+@staged_fit.register(DecisionTreeClassifier)
+def _staged_fit_dt(estimator: DecisionTreeClassifier, X, y):
+    path = estimator.cost_complexity_pruning_path(X, y)
+    for ccp_alpha in reversed(path.ccp_alphas):
+        estimator.set_params(ccp_alpha=ccp_alpha)
+        estimator.fit(X, y)
+        yield estimator
 
 
 class ProgressiveEnsemble(AbstractEnsemble):
@@ -133,6 +127,10 @@ class ProgressiveEnsemble(AbstractEnsemble):
         self : object
             Returns self.
         """
+        if self.steps is not numbers.Integral and self.steps <= 0:
+            raise ValueError(
+                f"steps size should be a strictly positive integer, was : {self.steps}"
+            )
 
         if isinstance(base_model, Pipeline):
             X = make_pipeline(base_model[:-1]).fit_transform(X, y)
@@ -141,39 +139,13 @@ class ProgressiveEnsemble(AbstractEnsemble):
             base_model = base_model
 
         base_model = clone(base_model)
-        probe_scorer = check_probe(probe)
+        probe = check_probe(probe)
 
-        if base_model.__class__ not in incremental_fit.registry:
-            raise ValueError(
-                f"{base_model.__class__.__name__} doesn't support iterative"
-                " learning. Register the estimator class to incremental_fit."
-            )
+        stages = staged_fit(base_model, X, y)
+        stages = islice(stages, None, None, self.steps)
+        probe_scores = np.stack([probe(stage, X, y) for stage in stages], axis=-1)
 
-        init = True
-        remaining_iterations = True
-        stages = []
-        while remaining_iterations:
-            base_model, remaining_iterations = incremental_fit(
-                base_model, X, y, remaining_iterations, init=init
-            )
-            init = False
-            stages.append(copy.deepcopy(base_model))
-
-        n_stages = len(stages)
-
-        if self.steps is not numbers.Integral and self.steps <= 0:
-            raise ValueError(
-                f"steps size should be a strictly positive integer, was : {self.steps}"
-            )
-
-        probe_scores = []
-
-        for i in range(0, n_stages, self.steps):
-            stage_probe_scores = probe_scorer(stages[i], X, y)
-            if stage_probe_scores.ndim == 1:
-                stage_probe_scores = np.expand_dims(stage_probe_scores, axis=1)
-            probe_scores.append(stage_probe_scores)
-
-        probe_scores = np.stack(probe_scores, axis=-1)
+        if probe_scores.ndim == 2:
+            probe_scores = probe_scores[:, None, :]
 
         return probe_scores, np.ones_like(probe_scores)
--- mislabeled/ensemble/tests/test_progressive.py
+++ mislabeled/ensemble/tests/test_progressive.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 import pytest
 from sklearn.datasets import make_classification
@@ -13,33 +11,26 @@ from mislabeled.probe._margin import soft_margin
     "estimator",
     [
         HistGradientBoostingClassifier(
-            early_stopping=False, max_iter=20, random_state=1
+            early_stopping=False, max_iter=100, random_state=1
         ),
         HistGradientBoostingClassifier(
             early_stopping=True, max_iter=100, random_state=1
         ),
-        GradientBoostingClassifier(n_estimators=5),
+        GradientBoostingClassifier(n_estimators=20),
     ],
 )
 def test_progressive_staged(estimator):
-    n_samples = int(1e5)
+    n_samples = int(1e4)
     X, y = make_classification(n_samples=n_samples)
-    detector_incr = AreaUnderMargin(estimator)
+    X = X.astype(np.float32)
 
-    start = time.perf_counter()
     estimator.fit(X, y)
-    staged_ts = []
-    for y_pred in estimator.staged_decision_function(X):
-        staged_ts.append(soft_margin(y, y_pred))
-    staged_ts = np.sum(staged_ts, axis=0)
-    end = time.perf_counter()
-    staged_time = end - start
+    baseline_ts = [
+        soft_margin(y, y_pred) for y_pred in estimator.staged_decision_function(X)
+    ]
+    baseline_ts = np.sum(baseline_ts, axis=0)
 
-    start = time.perf_counter()
+    detector_incr = AreaUnderMargin(estimator)
     incr_ts = detector_incr.trust_score(X, y)
-    end = time.perf_counter()
-    incr_time = end - start
 
-    print(staged_time, incr_time)
-
-    np.testing.assert_array_almost_equal(staged_ts, incr_ts)
+    np.testing.assert_array_almost_equal(baseline_ts, incr_ts, decimal=4)
