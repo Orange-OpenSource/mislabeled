417e5b082f7a6b795360c274d0914981de2fcb36
Pierre Nodet <pierre.nodet@orange.com>
2024-03-27 10:10:55 +0100

    compute the full hessian mama

--- mislabeled/probe/_influence.py
+++ mislabeled/probe/_influence.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
+from sklearn.model_selection import LeaveOneOut, cross_val_score
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.utils.extmath import safe_sparse_dot
 
@@ -11,51 +12,37 @@ def norm2(x, axis=1):
 
 
 class Influence:
-    """A template estimator to be used as a reference implementation.
 
-    For more information regarding how to build your own estimator, read more
-    in the :ref:`User Guide <user_guide>`.
-
-    Parameters
-    ----------
-    demo_param : str, default='demo_param'
-        A parameter used for demonstation of how to pass and store paramters.
-    """
+    def __init__(self, dampening=0):
+        self.dampening = dampening
 
     def __call__(self, estimator, X, y):
-        """A reference implementation of a fitting function.
-
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape (n_samples, n_features)
-            The training input samples.
-        y : array-like, shape (n_samples,) or (n_samples, n_outputs)
-            The target values (class labels in classification, real numbers in
-            regression).
-
-        Returns
-        -------
-        self : object
-            Returns self.
-        """
-
         if isinstance(estimator, Pipeline):
             X = make_pipeline(estimator[:-1]).transform(X)
-            coef = estimator[-1].coef_
-        else:
-            coef = estimator.coef_
+            estimator = estimator[-1]
 
-        # binary case
-        if coef.shape[0] == 1:
-            H = safe_sparse_dot(X, coef.T, dense_output=True)
-            H = np.ravel(H)
-            return H * (y - 0.5)
-        # multiclass case
-        else:
-            H = safe_sparse_dot(X, coef.T, dense_output=True)
-            mask = np.zeros_like(H, dtype=bool)
-            mask[np.arange(H.shape[0]), y] = True
-            return H[mask]
+        p = estimator.predict_proba(X)
+
+        n_samples, n_features = X.shape
+        n_classes = p.shape[1]
+
+        diff = np.copy(p)
+        diff[np.arange(n_samples), y] -= 1
+        grad = diff[:, :, None] * X[:, None, :]
+        grad = grad.reshape(n_samples, n_features * n_classes)
+
+        P = np.eye(n_classes) * p[:, None, :]
+        P -= p[:, None, :] * p[:, :, None]
+        XXt = X[:, None, :] * X[:, :, None]
+        H = P[:, :, None, :, None] * XXt[:, None, :, None, :]
+        H = H.reshape(n_samples, n_features * n_classes, n_features * n_classes)
+        H = np.mean(H, axis=0)
+        H += self.dampening * np.eye(n_features * n_classes)
+        H_inv = np.linalg.pinv(H)
+
+        influence = -grad @ H_inv @ grad.T
+
+        return np.diag(influence)
 
 
 class LinearGradNorm2:
