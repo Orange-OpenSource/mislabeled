3b1127c345d5415c7ace03df1d320f7fe515c188
Pierre Nodet <pierre.nodet@orange.com>
2024-05-12 19:41:00 +0200

    just some files reorganization

--- mislabeled/detect/__init__.py
+++ mislabeled/detect/__init__.py
@@ -1,3 +1,36 @@
-from ._model_based import ModelBasedDetector
+import operator
 
-__all__ = ["ModelBasedDetector"]
+from sklearn.base import BaseEstimator
+
+from mislabeled.aggregate import check_aggregate
+from mislabeled.probe import check_probe
+
+
+class ModelBasedDetector(BaseEstimator):
+    def __init__(self, base_model, ensemble, probe, aggregate):
+        self.base_model = base_model
+        self.ensemble = ensemble
+        self.probe = probe
+        self.aggregate = aggregate
+
+    def trust_score(self, X, y):
+        probe = check_probe(self.probe)
+        ensemble_probe_scores, kwargs = self.ensemble.probe_model(
+            self.base_model, X, y, probe
+        )
+        ensemble_probe_scores = (
+            (
+                operator.neg(probe_scores)
+                if hasattr(probe, "maximize") and not probe.maximize
+                else probe_scores
+            )
+            for probe_scores in ensemble_probe_scores
+        )
+        # probe_scores is an iterator of size e
+        # of numpy arrays of shape n x p
+        # n: #examples
+        # p: #probes
+        # e: #ensemble members
+
+        aggregate = check_aggregate(self.aggregate, **kwargs)
+        return aggregate(ensemble_probe_scores)
