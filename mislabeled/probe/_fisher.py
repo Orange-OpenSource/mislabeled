12570fb1c865c83919bb34016b57251eed7edf32
Pierre Nodet <pierre.nodet@orange.com>
2025-03-27 15:24:11 +0100

    init fisher

---
+++ mislabeled/probe/_fisher.py
@@ -0,0 +1,172 @@
+from functools import singledispatch, partial
+from itertools import chain
+from sklearn.base import is_classifier
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neural_network._multilayer_perceptron import DERIVATIVES
+import numpy as np
+from scipy.linalg import lu_solve, lu_factor
+from mislabeled.probe import LinearModel
+
+
+type MLP = MLPClassifier | MLPRegressor
+
+
+def jacobian(mlp: MLP, X: np.ndarray, block=False):
+    n_samples, n_features = X.shape
+    n_outputs = mlp.n_outputs_
+
+    layer_units = [n_features] + list(mlp.hidden_layer_sizes) + [n_outputs]
+
+    # Initialize lists
+    activations = [X] + [None] * (len(layer_units) - 1)
+    deltas = [None] * len(activations)
+    coef_grads = [None] * (len(layer_units) - 1)
+    intercept_grads = [None] * (len(layer_units) - 1)
+
+    # Forward
+    activations = mlp._forward_pass(activations)
+
+    # Backward
+    if mlp.out_activation_ == "softmax":
+        deltas[-1] = activations[-1][:, :, None] * (
+            np.eye(n_outputs, dtype=X.dtype)[None, ...] - activations[-1][:, None, :]
+        )
+    elif mlp.out_activation_ == "logistic":
+        deltas[-1] = (activations[-1] * (1 - activations[-1]))[..., None]
+    else:
+        deltas[-1] = np.broadcast_to(
+            np.eye(n_outputs, dtype=X.dtype), (n_samples, n_outputs, n_outputs)
+        )
+
+    coef_grads[-1] = (
+        deltas[-1][:, :, None, :] * activations[-2][:, None, :, None]
+    ).reshape(n_samples, n_outputs, -1)
+    intercept_grads[-1] = deltas[-1]
+
+    inplace_derivative = DERIVATIVES[mlp.activation]
+    # Iterate over the hidden layers
+    for i in range(mlp.n_layers_ - 2, 0, -1):
+        deltas[i] = deltas[i + 1] @ mlp.coefs_[i].T[None, :, :]
+        inplace_derivative(
+            np.broadcast_to(activations[i][:, None, :], deltas[i].shape), deltas[i]
+        )
+
+        coef_grads[i - 1] = (
+            deltas[i][:, :, None, :] * activations[i - 1][:, None, :, None]
+        ).reshape(n_samples, n_outputs, -1)
+        intercept_grads[i - 1] = deltas[i]
+
+    if block:
+        return list(
+            map(partial(np.concatenate, axis=-1), zip(coef_grads, intercept_grads))
+        )
+    else:
+        return np.concatenate(
+            list(chain(*zip(coef_grads, intercept_grads))), axis=-1, dtype=X.dtype
+        )
+
+
+def fisher(mlp: MLP, X: np.ndarray):
+    J = jacobian(mlp, X)
+    diagΣ = diag_var(mlp, X)
+    F = np.einsum("ijk, ij, ijl -> kl", J, 1 / diagΣ, J, optimize="optimal")
+    return F
+
+
+def block_fisher(mlp: MLP, X: np.ndarray) -> list[np.ndarray]:
+    J = jacobian(mlp, X, block=True)
+    diagΣ = diag_var(mlp, X)
+    F = [np.einsum("ijk, ij, ijl -> kl", j, 1 / diagΣ, j, optimize=True) for j in J]
+    return F
+
+
+def forward(mlp: MLP, X: np.ndarray, raw=True):
+    if raw:
+        tmp = mlp.out_activation_
+        mlp.out_activation_ = "identity"
+    predictions = mlp._forward_pass_fast(X, check_input=False)
+    if raw:
+        mlp.out_activation_ = tmp
+    return predictions
+
+
+class MLPLinearModel(LinearModel):
+    def __init__(self, mlp, loss, regul):
+        self.mlp = mlp
+        super().__init__(True, True, loss, regul)
+
+    def decision_function(self, X):
+        return forward(self.mlp, X)
+
+    def predict_proba(self, X):
+        return forward(self.mlp, X, raw=False)
+
+    def hessian(self, X, y):
+        F = fisher(self.mlp, X)
+        F[np.diag_indices_from(F)] += self.regul
+        return F
+
+    def jacobian(self, X, y):
+        return jacobian(self.mlp, X)
+
+    def diag_hat_matrix(self, X, y):
+        Σinv = np.sqrt(1 / diag_var(self.mlp, X))
+        J = self.jacobian(X, y)
+        F = self.hessian(X, y)
+        J = Σinv[..., None] * J
+        F_LU = lu_factor(F)
+        return J @ lu_solve(F_LU, J.transpose(2, 0, 1)).transpose(1, 0, 2)
+
+    def grad_p(self, X, y):
+        Vinv = self.inverse_variance(self.predict_proba(X))
+        r = self.grad_y(X, y)
+        J = self.jacobian(X, y)
+        return (r[:, None, :] @ Vinv @ J)[:, 0, :]
+
+    def grad_y(self, X, y):
+        return self.predict_proba(X) - y
+
+    @property
+    def in_dim(self):
+        return self.mlp.coefs_[0].shape[0]
+
+    @property
+    def out_dim(self):
+        return self.mlp.coefs_[-1].shape[-1]
+
+
+def linearize_mlp_fisher(estimator, X, y):
+    if is_classifier(estimator):
+        loss = "log_loss"
+        y = LabelBinarizer().fit(estimator.classes_).transform(y)
+    else:
+        loss = "l2"
+        if y.ndim == 1:
+            y = y.reshape(-1, 1)
+
+    if estimator.solver == "lbfgs":
+        batch_size = X.shape[0]
+    elif estimator.batch_size == "auto":
+        batch_size = min(200, X.shape[0])
+    else:
+        batch_size = estimator.batch_size
+
+    if not estimator.solver == "lbfgs":
+        regul = estimator.alpha * batch_size / X.shape[0]
+    else:
+        regul = estimator.alpha
+
+    return MLPLinearModel(estimator, loss, regul), X, y
+
+
+def ntf(mlp: MLP, X: np.ndarray):
+    jac_X = jacobian(mlp, X)
+    return jac_X.reshape(X.shape[0], -1)
+
+
+def ntk(mlp: MLP, X: np.ndarray, Y: np.ndarray | None = None):
+    jac_X = jacobian(mlp, X)
+    jac_Y = jac_X if Y is None else jacobian(mlp, Y)
+
+    return np.einsum("ikl, jkl->ij", jac_X, jac_Y, optimize=True)
---
+++ tests/probe/test_fisher.py
@@ -0,0 +1,242 @@
+from itertools import chain
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.datasets import make_regression, make_classification, make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import is_classifier
+from scipy import differentiate
+import numpy as np
+import pytest
+from deepgnostics.ntk import (
+    MLPLinearModel,
+    fisher,
+    linearize_mlp_fisher,
+    ntk,
+    MLP,
+    jacobian,
+    num_params,
+)
+from mislabeled.probe import linearize
+from mislabeled.probe._linear import linearize_mlp
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [MLPRegressor(hidden_layer_sizes=(20,)), MLPClassifier(hidden_layer_sizes=(20,))],
+)
+def test_ntk_Y_none(mlp):
+    if is_classifier(mlp):
+        X, y = make_classification()
+    else:
+        X, y = make_regression(n_features=10)
+    X = StandardScaler().fit_transform(X)
+    mlp.set_params(
+        max_iter=1, solver="sgd", learning_rate="constant", learning_rate_init=1e-8
+    ).fit(X, y)
+    np.testing.assert_allclose(ntk(mlp, X, None), ntk(mlp, X, X))
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [MLPRegressor(hidden_layer_sizes=(20,)), MLPClassifier(hidden_layer_sizes=(20,))],
+)
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_ntk_size(mlp, outputs):
+    if is_classifier(mlp):
+        X, y = make_blobs(centers=max(2, outputs))
+    else:
+        X, y = make_regression(n_features=20, n_targets=outputs)
+    X = StandardScaler().fit_transform(X)
+    Y = X[0:10]
+    mlp.set_params(
+        max_iter=1, solver="sgd", learning_rate="constant", learning_rate_init=1e-8
+    ).fit(X, y)
+    assert ntk(mlp, X, Y).shape == (X.shape[0], Y.shape[0])
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [
+        MLPRegressor(hidden_layer_sizes=(30, 40)),
+        MLPClassifier(hidden_layer_sizes=(30, 40)),
+    ],
+)
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_jacobian_size(mlp: MLP, outputs):
+    if is_classifier(mlp):
+        X, y = make_blobs(centers=max(2, outputs))
+    else:
+        X, y = make_regression(n_features=20, n_targets=outputs)
+    X = StandardScaler().fit_transform(X)
+    mlp.fit(X, y)
+    assert jacobian(mlp, X).shape == (X.shape[0], outputs, num_params(mlp))
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [
+        MLPRegressor(hidden_layer_sizes=(30, 40)),
+        MLPClassifier(hidden_layer_sizes=(30, 40)),
+    ],
+)
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_fisher_size(mlp: MLP, outputs):
+    if is_classifier(mlp):
+        X, y = make_blobs(centers=max(2, outputs))
+    else:
+        X, y = make_regression(n_features=20, n_targets=outputs)
+    X = StandardScaler().fit_transform(X)
+    mlp.fit(X, y)
+    assert fisher(mlp, X).shape == (num_params(mlp), num_params(mlp))
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [
+        MLPRegressor(hidden_layer_sizes=(30, 40)),
+        MLPClassifier(hidden_layer_sizes=(30, 40)),
+    ],
+)
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_fisher_linearization(mlp: MLP, outputs):
+    if is_classifier(mlp):
+        X, y = make_blobs(centers=max(2, outputs))
+    else:
+        X, y = make_regression(n_features=20, n_targets=outputs)
+    X = StandardScaler().fit_transform(X)
+    mlp.fit(X, y)
+    linearize.register(type(mlp), linearize_mlp_fisher)
+    linearized, X_lin, y_lin = linearize(mlp, X, y)
+    np.testing.assert_allclose(X_lin, X)
+    assert isinstance(linearized, MLPLinearModel)
+    assert linearized.hessian(X_lin, y_lin).shape == (num_params(mlp), num_params(mlp))
+    assert linearized.grad_y(X_lin, y_lin).shape == (X.shape[0], linearized.out_dim)
+    assert linearized.grad_p(X_lin, y_lin).shape == (X.shape[0], num_params(mlp))
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [
+        MLPRegressor(hidden_layer_sizes=[], alpha=1),
+        MLPClassifier(hidden_layer_sizes=[], alpha=1),
+    ],
+)
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_fisher_equals_hessian_last_layer_for_depth0(mlp: MLP, outputs):
+    if is_classifier(mlp):
+        if outputs > 2:
+            return
+        X, y = make_blobs(centers=max(2, outputs))
+    else:
+        X, y = make_regression(n_features=20, n_targets=outputs)
+    X = StandardScaler().fit_transform(X)
+    mlp.fit(X, y)
+    linearize.register(type(mlp), linearize_mlp)
+    elinearized, Xe, ye = linearize(mlp, X, y)
+    linearize.register(type(mlp), linearize_mlp_fisher)
+    flinearized, Xf, yf = linearize(mlp, X, y)
+    eh = elinearized.hessian(Xe, ye)
+    for i in range(1, outputs + 1):
+        eh[-i, -i] += elinearized.regul
+    np.testing.assert_allclose(eh, flinearized.hessian(Xf, yf), strict=True)
+    np.testing.assert_allclose(
+        elinearized.diag_hat_matrix(Xe, ye),
+        flinearized.diag_hat_matrix(Xf, yf),
+        strict=True,
+        atol=1e-2,
+    )  # TODO: why is this different ?
+    np.testing.assert_allclose(
+        elinearized.grad_p(Xe, ye), flinearized.grad_p(Xf, yf), strict=True
+    )
+    np.testing.assert_allclose(
+        elinearized.grad_y(Xe, ye), flinearized.grad_y(Xf, yf), strict=True
+    )
+
+
+@pytest.mark.parametrize(
+    "mlp",
+    [
+        MLPRegressor(hidden_layer_sizes=()),
+        MLPClassifier(hidden_layer_sizes=()),
+        MLPRegressor(hidden_layer_sizes=(10,), activation="identity"),
+        MLPClassifier(hidden_layer_sizes=(10,), activation="identity"),
+        MLPRegressor(hidden_layer_sizes=(10,)),
+        MLPClassifier(hidden_layer_sizes=(10,)),
+        MLPRegressor(hidden_layer_sizes=(4, 4, 4)),
+        MLPClassifier(hidden_layer_sizes=(4, 4, 4)),
+    ],
+)
+@pytest.mark.parametrize("init", [True, False])
+@pytest.mark.parametrize("outputs", [1, 4])
+def test_jacobian_finite_diff(mlp, init, outputs):
+    if is_classifier(mlp):
+        X, y = make_blobs(10000, n_features=2, centers=max(2, outputs), random_state=1)
+    else:
+        X, y = make_regression(10000, n_features=2, n_targets=outputs, random_state=1)
+    X = StandardScaler().fit_transform(X)
+    if y.ndim == 0:
+        y = y.reshape(1)
+    if init:
+        mlp.set_params(
+            max_iter=1, solver="sgd", learning_rate="constant", random_state=1
+        ).fit(X, y)
+    else:
+        mlp.set_params(random_state=1).fit(X, y)
+
+    packed_raveled_coef = np.concatenate(
+        list(
+            chain.from_iterable(
+                map(lambda c, i: [c.ravel(), i], mlp.coefs_, mlp.intercepts_)
+            )
+        ),
+        axis=0,
+    )
+
+    def unpack_unravel(packed_raveled_coef, layer_units):
+        idx = 0
+        coefs = []
+        intercepts = []
+        for i in range(len(layer_units) - 1):
+            size = layer_units[i] * layer_units[i + 1]
+            coefs.append(
+                packed_raveled_coef[idx : idx + size].reshape(
+                    layer_units[i], layer_units[i + 1]
+                )
+            )
+            idx += size
+            intercepts.append(packed_raveled_coef[idx : idx + layer_units[i + 1]])
+            idx += layer_units[i + 1]
+        return coefs, intercepts
+
+    layer_units = [X.shape[1]] + list(mlp.hidden_layer_sizes) + [mlp.n_outputs_]
+
+    def vectorized_predict_proba(packed_raveled_coef):
+        def f(prc):
+            coefs, intercepts = unpack_unravel(prc, layer_units)
+            mlp.coefs_ = coefs
+            mlp.intercepts_ = intercepts
+            if is_classifier(mlp):
+                if len(np.unique(y)) > 2:
+                    return mlp.predict_proba(X).sum(axis=0)
+                else:
+                    return mlp.predict_proba(X)[:, 1].sum(axis=0)
+            else:
+                return mlp.predict(X).sum(axis=0)
+
+        return np.apply_along_axis(f, axis=0, arr=packed_raveled_coef)
+
+    with np.printoptions(precision=4, suppress=True):
+        print(empJ := jacobian(mlp, X).mean(axis=0))
+        print(
+            J := differentiate.jacobian(
+                vectorized_predict_proba, packed_raveled_coef
+            ).df
+            / X.shape[0]
+        )
+
+    np.testing.assert_allclose(
+        empJ,
+        J if J.ndim > 1 else J.reshape(1, -1),
+        rtol=1e-1,
+        atol=1e-1,
+        strict=True,
+    )
